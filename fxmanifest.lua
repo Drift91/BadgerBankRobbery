4ecde8b022502efdd4481673b38536e35c91ed24
Kyle J. McKeown <mckeown_k@hotmail.com>
2024-11-20 01:11:50 -0500

    Update to fxmanifest v2

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-----------------------------------
+-------- BadgerBankRobbery --------
+---          by Badger          ---
+-----------------------------------
+
+fx_version "cerulean"
+game "gta5"
+
+author "JaredScar"
+url "https://github.com/JaredScar/BadgerBankRobbery"
+
+shared_script "config.lua"
+client_script "client.lua"
+server_script "server.lua"
